ffd8aa3370e85380242a7e0b35c93dc2ee2b1504
harshita <harshita1225@gmail.com>
2022-07-14 06:50:41 +0200

    task completed

---
+++ solution.js
@@ -0,0 +1,25 @@
+//task -1
+
+let i = 1;
+while (i <= 15) {
+  console.log(i);
+  i++;
+}
+
+//task -2
+let num = 0;
+let sum = 0;
+while (num <= 20) {
+  sum += num;
+  num++;
+}
+console.log("the sum is:", sum);
+
+//task -3 do while
+let x = 0;
+do {
+  console.log("the number is:", [x]);
+  x++;
+} while (x < 20);
+
+//task-4
